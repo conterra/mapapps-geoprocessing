9fdd1b9b72fd170606d9b8b11bdaf1aa987f02d9
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-06-25 10:56:22 +0200

    Add copyright and license information to AddFeaturesToLayerAction and api files

--- src/main/js/bundles/dn_geoprocessing/actions/AddFeaturesToLayerAction.ts
+++ src/main/js/bundles/dn_geoprocessing/actions/AddFeaturesToLayerAction.ts
@@ -13,6 +13,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
+
 import { Action } from "map-actions/api";
 import Layer from "esri/layers/Layer";
 
